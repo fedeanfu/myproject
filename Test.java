395c587454280b805836172771fdd1983bb86d28
fedeanfu <federica.anfuso@gmail.com>
2023-05-23 18:43:45 +0200

    Added a new hallo in italian because italians dont usually know how to speak english

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello from my project");
         System.out.println("Buonasera agli italiani in ascolto");
+
+        System.out.println("------------------------");
     }
 }
